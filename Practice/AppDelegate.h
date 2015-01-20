b572b82981ba1c28e0d4e364fd1ee81449690863
alancodvo <alancodvo@gmail.com>
2015-01-20 19:11:31 +0900

    Initial Commit

---
+++ Practice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Practice
+//
+//  Created by alan-arakawa-yoshihiro on H27/01/20.
+//  Copyright (c) 平成27年 Yoshihiro Arakawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
