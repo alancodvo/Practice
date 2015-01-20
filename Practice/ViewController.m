0ea7a49faf828f8391c0b3aeaaf9febba6c57c03
alancodvo <alancodvo@gmail.com>
2015-01-20 19:50:54 +0900

    テキストを「hello」に変更

--- Practice/ViewController.m
+++ Practice/ViewController.m
@@ -18,7 +18,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    self.firstLabel.text = @"こんにちは";
+    self.firstLabel.text = @"こんにちは"; // テキストをこんにちはに変更
+    [self.firstLabel setText:@"hello"]; // テキストをhelloに変更
 }
 
 - (void)didReceiveMemoryWarning {
